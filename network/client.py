127de640a5ce28e9584cf0808515addd929af434
Angel Dario <dariogomez1011@gmail.com>
2022-07-03 21:23:30 -0400

    connection to any host bug fixed

--- network/client.py
+++ network/client.py
@@ -1,10 +1,12 @@
 import socket
 import os
+from datetime import datetime
 
 BUFFER_SIZE = 4096
 
 def connectToHost(mainWindow, SERVER_HOST, SERVER_PORT):
     connectionStablished = False
+    fileError = False
     socket_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         socket_s.connect((SERVER_HOST, SERVER_PORT))
@@ -18,12 +20,19 @@ def connectToHost(mainWindow, SERVER_HOST, SERVER_PORT):
         filename = "network/log.txt"
         with open(filename, "wb") as file:
             while True:
-                bytes_read = socket_s.recv(BUFFER_SIZE)
-                if not bytes_read:
+                try:
+                    bytes_read = socket_s.recv(BUFFER_SIZE)
+                    if not bytes_read:
+                        break
+                    file.write(bytes_read)
+                except Exception as e:
+                    print(str(e))
+                    fileError = True
                     break
-                file.write(bytes_read)
-        
-        mainWindow.ui.console.insertPlainText(f"CONSOLA >> Archivo recibido.\r")
+                    
+        if(not fileError):
+            mainWindow.ui.console.insertPlainText(f"CONSOLA >> Archivo recibido.\r")
         socket_s.close()
+        mainWindow.ui.console.insertPlainText(f"{datetime.today().strftime('%Y-%m-%d %H:%M:%S')} >> Conexión cerrada con el servidor >> {SERVER_HOST} : {SERVER_PORT}\r")
         mainWindow.ui.connectBtn.setEnabled(True)
     
