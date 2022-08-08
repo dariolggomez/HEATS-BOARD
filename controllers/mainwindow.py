49dd4894c102296cde5bff9398bd5379978c983f
Angel Dario <dariogomez1011@gmail.com>
2022-08-08 16:46:32 -0400

    created dashboard controller and moved load graphics function

--- controllers/mainwindow.py
+++ controllers/mainwindow.py
@@ -17,7 +17,8 @@ from matplotlib.figure import Figure
 from threading import Thread, Timer
 import matplotlib.pyplot as plt
 from sqlalchemy import true
-import controllers.login as Login 
+import controllers.login as Login
+from controllers.dashboard import DashboardController 
 import services.user_service as user_service
 import network.client as client
 from controllers.singleton import SingletonClass
@@ -148,7 +149,7 @@ class MainWindow(QMainWindow):
 
             self.ui.console.insertPlainText(f"{datetime.today().strftime('%Y-%m-%d %H:%M:%S')} >> HEATS-BOARD Inicializado.\r")
             #LOAD DASHBOARD GRAPHICS
-            self.loadGraphics()
+            DashboardController.loadGraphics(self)
             
             # LOAD SETTINGS
             try:    
@@ -195,7 +196,7 @@ class MainWindow(QMainWindow):
         if btnWidget.objectName() == "btn_home":
             self.ui.stackedWidget.setCurrentWidget(self.ui.page_home)
             if(self.graphicsLoaded == False):
-                self.loadGraphics()
+                DashboardController.loadGraphics(self)
             self.resetStyle("btn_home")
             self.labelPage("Dashboard")
             btnWidget.setStyleSheet(self.selectMenu(btnWidget.styleSheet()))
@@ -234,114 +235,6 @@ class MainWindow(QMainWindow):
             login.show()
             login.activateWindow()
             login.raise_()
-
-    def loadGraphics(self):
-        localFileReaded = False
-        try:
-            data = pd.read_csv("network/log.txt", nrows=20, sep="/", header=None)
-            localFileReaded = True
-        except:
-            self.ui.console.insertPlainText(f"{datetime.today().strftime('%Y-%m-%d %H:%M:%S')} >> No se encontró el archivo proveniente de HEATS-NET. Debe conectarse al servidor.\r")
-            self.ui.console.setFocus()
-        if(not localFileReaded):
-            try:
-                data = pd.read_csv("backup/log.txt", nrows=20, sep="/", header=None)
-                self.ui.console.insertPlainText(f"{datetime.today().strftime('%Y-%m-%d %H:%M:%S')} >> Se cargará la última información guardada.\r")
-            except:
-                self.ui.console.insertPlainText(f"{datetime.today().strftime('%Y-%m-%d %H:%M:%S')} >> No se encontró el archivo salva, no se podrá mostrar información.\r")
-                self.ui.console.setFocus()
-                return
-        if(data is not None and self.graphicsLoaded == False):
-            dataToDisplay = []
-            for x in range(300,500):
-                dataToDisplay.append(data.iloc[0][x])
-
-            self.static_canvas = FigureCanvas(Figure(figsize=(5, 3)))
-            self.ui.chartLayout1.addWidget(NavigationToolbar(self.static_canvas, self))
-            self.ui.chartLayout1.addWidget(self.static_canvas)
-
-            self._static_ax = self.static_canvas.figure.subplots()
-            self._static_ax.set_title("Frecuencia | Tiempo")
-            self._static_ax.set_ylabel("Frecuencia")
-            self._static_ax.set_xlabel("Tiempo")
-            # self._static_ax.yaxis.set_visible(False)
-            x = np.linspace(0, len(dataToDisplay)-1, len(dataToDisplay))
-            y = np.array(dataToDisplay)
-            # self._static_ax.scatter()
-            self._static_ax.plot(x, y)
-
-            # Dynamic Chart
-            self.dynamic_canvas = FigureCanvas(Figure(figsize=(5, 3)))
-            self.ui.chartLayout2.addWidget(NavigationToolbar(self.dynamic_canvas, self))
-            self.ui.chartLayout2.addWidget(self.dynamic_canvas)
-
-            self._dynamic_ax = self.dynamic_canvas.figure.subplots()
-            self._dynamic_ax.set_title("Frecuencias Críticas y Fatales")
-            self._dynamic_ax.set_ylabel("Frecuencia")
-            self._dynamic_ax.set_xlabel("Tiempo")
-            dataFiltered = dataToDisplay.copy()
-            dataToFilter = dataToDisplay.copy()
-            for value in dataToFilter:
-                if(value <= 40):
-                    dataFiltered.remove(value)
-            xDynamic = np.linspace(0, len(dataFiltered)-1, len(dataFiltered))
-            yDynamic = np.array(dataFiltered)
-            self._dynamic_ax.plot(xDynamic,yDynamic)
-            # Set up a Line2D.
-            # self._line, = self._dynamic_ax.plot(t, np.sin(t + time.time()))
-            # self._timer = dynamic_canvas.new_timer(6000)
-            # self._timer.add_callback(self._update_canvas)
-            # self._timer.start()
-            
-            # Pie Chart
-            labels = 'Parámetros Críticos', 'Parámetros de Alerta', 'Parámetros Normales', 'Parámetros Fatales'
-            sizeNormalParameters,countNormalParameters = self.countNormalParams(dataToDisplay)
-            sizeAlertParameters,countAlertParameters = self.countAlertParams(dataToDisplay)
-            sizeCriticalParameters,countCriticalParameters = self.countCriticalParams(dataToDisplay)
-            sizeFatalParameters,countFatalParameters = self.countFatalParams(dataToDisplay)
-            # print(sizeNormalParameters)
-            # print(sizeAlertParameters)
-            # print(sizeCriticalParameters)
-            # print(sizeFatalParameters)
-            sizes = [sizeCriticalParameters, sizeAlertParameters, sizeNormalParameters, sizeFatalParameters]
-            explode = (0, 0, 0.1, 0)
-            self.static_canvas_pie = FigureCanvas(Figure(figsize=(5, 3)))
-            self.ui.chartLayout3.addWidget(self.static_canvas_pie)
-            self.ui.chartLayout3.addWidget(NavigationToolbar(self.static_canvas_pie, self))
-
-            self._pie_ax = self.static_canvas_pie.figure.subplots()
-            self._pie_ax.set_title("Porcentaje por Criticidad de los Valores")
-            self._pie_ax.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
-            shadow=True, startangle=90)
-            self._pie_ax.axis('equal')
-            self._pie_ax.plot()
-
-            #Bar Chart 
-            labels = ['Normales', 'Alerta', 'Críticos', 'Fatales']
-            parameters = [countNormalParameters, countAlertParameters, countCriticalParameters, countFatalParameters]
-            # women_means = [25, 32, 34, 20,]
-
-            x = np.arange(len(labels))  # label locations
-            width = 0.35  # width of the bars
-
-            self.static_canvas_lines = FigureCanvas(Figure(figsize=(5,4)))
-            self.ui.chartLayout4.addWidget(self.static_canvas_lines)
-            self.ui.chartLayout4.addWidget(NavigationToolbar(self.static_canvas_lines,self))
-            self.lines_ax = self.static_canvas_lines.figure.subplots()
-            rects1 = self.lines_ax.bar(x, parameters, width, label='Parámetros')
-            # rects2 = self.lines_ax.bar(x + width/2, women_means, width, label='Femenino')
-
-            self.lines_ax.set_ylabel('Valor')
-            self.lines_ax.set_title('Parámetros por Criticidad')
-            self.lines_ax.set_xticks(x, labels)
-            self.lines_ax.legend()
-
-            self.lines_ax.bar_label(rects1, padding=3)
-            # self.lines_ax.bar_label(rects2, padding=3)
-
-            self.static_canvas_lines.figure.tight_layout()
-            self.lines_ax.plot()
-            self.graphicsLoaded = True
         
     def countNormalParams(self, data):
         count = 0
